138bce61ff0267e10bb8810c2f76e8a776f18338
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-19 12:45:03 +0700

    Update Duel.php

--- src/Duels/Duel.php
+++ src/Duels/Duel.php
@@ -2,49 +2,62 @@
 
 namespace Duels;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\Player;
-use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase implements Listener {
+class Duel {
+    private Player $player1;
+    private Player $player2;
+    private int $duration;
 
-    private DuelManager $duelManager;
-
-    public function onEnable(): void {
-        $this->duelManager = new DuelManager($this);
-        $this->getServer()->getPluginManager()->registerEvents($this->duelManager, $this);
-        $this->getLogger()->info(TextFormat::GREEN . "Duels plugin enabled!");
+    public function __construct(Player $player1, Player $player2, int $duration) {
+        $this->player1 = $player1;
+        $this->player2 = $player2;
+        $this->duration = $duration;
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        switch ($command->getName()) {
-            case "duel":
-                if ($sender instanceof Player) {
-                    $this->duelManager->showDuelForm($sender);
-                } else {
-                    $sender->sendMessage(TextFormat::RED . "Perintah ini hanya dapat digunakan oleh pemain dalam permainan.");
-                }
-                return true;
+    public function start(): void {
+        // Reset inventory
+        $this->preparePlayer($this->player1);
+        $this->preparePlayer($this->player2);
+        
+        // Inform players
+        $this->player1->sendMessage(TextFormat::GREEN . "Duel dimulai melawan " . $this->player2->getName());
+        $this->player2->sendMessage(TextFormat::GREEN . "Duel dimulai melawan " . $this->player1->getName());
 
-            case "accept":
-                if ($sender instanceof Player) {
-                    return $this->duelManager->handleAcceptCommand($sender);
-                }
-                return false;
-
-            case "decline":
-                if ($sender instanceof Player) {
-                    return $this->duelManager->handleDeclineCommand($sender);
-                }
-                return false;
-        }
-        return false;
+        // Schedule ending the duel
+        $this->player1->getServer()->getScheduler()->scheduleDelayedTask(new \pocketmine\scheduler\ClosureTask(function() {
+            $this->endDuel();
+        }), $this->duration * 20); // 20 ticks per second
     }
 
-    public function getDuelManager(): DuelManager {
-        return $this->duelManager;
+    private function preparePlayer(Player $player): void {
+        // Reset inventory
+        $player->getInventory()->clearAll();
+        
+        // Give items (armor and weapons)
+        $player->getInventory()->addItem(\pocketmine\item\ItemFactory::getInstance()->get(276, 0, 1)); // Diamond Sword
+        // Give diamond armor
+        $player->getInventory()->setArmorContents([
+            \pocketmine\item\ItemFactory::getInstance()->get(311, 0, 1), // Diamond Helmet
+            \pocketmine\item\ItemFactory::getInstance()->get(307, 0, 1), // Diamond Chestplate
+            \pocketmine\item\ItemFactory::getInstance()->get(308, 0, 1), // Diamond Leggings
+            \pocketmine\item\ItemFactory::getInstance()->get(309, 0, 1)  // Diamond Boots
+        ]);
+    }
+
+    private function endDuel(): void {
+        // Logic to handle duel ending
+        // Notify players that the duel has ended
+        $this->player1->sendMessage(TextFormat::YELLOW . "Duel berakhir!");
+        $this->player2->sendMessage(TextFormat::YELLOW . "Duel berakhir!");
+
+        // Clear inventory or return players to original state if necessary
+        $this->resetPlayers();
+    }
+
+    private function resetPlayers(): void {
+        $this->player1->getInventory()->clearAll();
+        $this->player2->getInventory()->clearAll();
     }
 }
