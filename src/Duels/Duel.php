a4a5117656c0635099a7a90faed9c3b92af9ee31
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-19 12:56:25 +0700

    Update Duel.php

--- src/Duels/Duel.php
+++ src/Duels/Duel.php
@@ -1,63 +1,37 @@
-<?php
-
 namespace Duels;
 
 use pocketmine\Player;
-use pocketmine\utils\TextFormat;
 
 class Duel {
     private Player $player1;
     private Player $player2;
     private int $duration;
 
-    public function __construct(Player $player1, Player $player2, int $duration) {
+    public function __construct(Player $player1, Player $player2, int $duration = 60) {
         $this->player1 = $player1;
         $this->player2 = $player2;
         $this->duration = $duration;
     }
 
     public function start(): void {
-        // Reset inventory
         $this->preparePlayer($this->player1);
         $this->preparePlayer($this->player2);
         
-        // Inform players
-        $this->player1->sendMessage(TextFormat::GREEN . "Duel dimulai melawan " . $this->player2->getName());
-        $this->player2->sendMessage(TextFormat::GREEN . "Duel dimulai melawan " . $this->player1->getName());
-
-        // Schedule ending the duel
-        $this->player1->getServer()->getScheduler()->scheduleDelayedTask(new \pocketmine\scheduler\ClosureTask(function() {
-            $this->endDuel();
-        }), $this->duration * 20); // 20 ticks per second
+        $this->player1->sendMessage("Duel dimulai dengan " . $this->player2->getName() . " selama " . $this->duration . " detik!");
+        $this->player2->sendMessage("Duel dimulai dengan " . $this->player1->getName() . " selama " . $this->duration . " detik!");
+        
+        // Logika duel yang sebenarnya akan ditempatkan di sini
     }
 
     private function preparePlayer(Player $player): void {
-        // Reset inventory
-        $player->getInventory()->clearAll();
+        $inventory = $player->getInventory();
+        $inventory->clearAll();
         
-        // Give items (armor and weapons)
-        $player->getInventory()->addItem(\pocketmine\item\ItemFactory::getInstance()->get(276, 0, 1)); // Diamond Sword
-        // Give diamond armor
-        $player->getInventory()->setArmorContents([
-            \pocketmine\item\ItemFactory::getInstance()->get(311, 0, 1), // Diamond Helmet
-            \pocketmine\item\ItemFactory::getInstance()->get(307, 0, 1), // Diamond Chestplate
-            \pocketmine\item\ItemFactory::getInstance()->get(308, 0, 1), // Diamond Leggings
-            \pocketmine\item\ItemFactory::getInstance()->get(309, 0, 1)  // Diamond Boots
-        ]);
-    }
-
-    private function endDuel(): void {
-        // Logic to handle duel ending
-        // Notify players that the duel has ended
-        $this->player1->sendMessage(TextFormat::YELLOW . "Duel berakhir!");
-        $this->player2->sendMessage(TextFormat::YELLOW . "Duel berakhir!");
-
-        // Clear inventory or return players to original state if necessary
-        $this->resetPlayers();
-    }
-
-    private function resetPlayers(): void {
-        $this->player1->getInventory()->clearAll();
-        $this->player2->getInventory()->clearAll();
+        // Memberikan senjata dan armor
+        $inventory->addItem(\pocketmine\item\ItemFactory::getInstance()->get(276, 0, 1)); // Diamond Sword
+        $inventory->addItem(\pocketmine\item\ItemFactory::getInstance()->get(310, 0, 1)); // Diamond Helmet
+        $inventory->addItem(\pocketmine\item\ItemFactory::getInstance()->get(311, 0, 1)); // Diamond Chestplate
+        $inventory->addItem(\pocketmine\item\ItemFactory::getInstance()->get(312, 0, 1)); // Diamond Leggings
+        $inventory->addItem(\pocketmine\item\ItemFactory::getInstance()->get(313, 0, 1)); // Diamond Boots
     }
 }
