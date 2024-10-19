6d95f5b2d87813f25b231315b8557a9f434deb5c
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-19 12:08:29 +0700

    Update Main.php

--- src/Duels/Main.php
+++ src/Duels/Main.php
@@ -7,7 +7,6 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\event\Listener;
-use pocketmine\form\Form;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener {
@@ -17,6 +16,7 @@ class Main extends PluginBase implements Listener {
     public function onEnable(): void {
         $this->duelManager = new DuelManager($this);
         $this->getServer()->getPluginManager()->registerEvents($this->duelManager, $this);
+        $this->getLogger()->info(TextFormat::GREEN . "Duels plugin enabled!");
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
@@ -28,9 +28,20 @@ class Main extends PluginBase implements Listener {
                     $sender->sendMessage(TextFormat::RED . "Perintah ini hanya dapat digunakan oleh pemain dalam permainan.");
                 }
                 return true;
-            default:
+
+            case "accept":
+                if ($sender instanceof Player) {
+                    return $this->duelManager->handleAcceptCommand($sender);
+                }
+                return false;
+
+            case "decline":
+                if ($sender instanceof Player) {
+                    return $this->duelManager->handleDeclineCommand($sender);
+                }
                 return false;
         }
+        return false;
     }
 
     public function getDuelManager(): DuelManager {
